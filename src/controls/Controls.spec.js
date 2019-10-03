03e094a1e4e5241c6e870072c88d9e4508a1a938
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-10-03 00:43:11 -0400

    Made some chanes to Controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,4 +1,18 @@
 // Test away!
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
 import Controls from "../controls/Controls";
+
+test("can it unlocked and open", () => {
+  const closeLookout = jest.fn();
+  const { queryByText } = render(
+    <Controls locked={false} toggleClosed={closeLookout} />
+  );
+  const buttonLock = queryByText("Lock Gate");
+  expect(buttonLock);
+  expect(buttonLock.disabled).toBe(true);
+  const closeButton = queryByText("Close Gate");
+  expect(closeButton);
+  expect(closeButton.disabled).toBe(false);
+});
