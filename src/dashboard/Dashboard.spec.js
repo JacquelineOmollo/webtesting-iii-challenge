c2c8cd544ecf84e8f6d897250ddf7b278abde0cb
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-10-03 23:39:33 -0400

    started DashBoard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,7 +1,24 @@
-// Test away
-// import React from "react";
-// import "@testing-library/jest-dom/extend-expect";
-// import { render, fireEvent } from "@testing-library/react";
-// import Dashboard from "../dashboard/Dashboard";
+//Test away
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent } from "@testing-library/react";
+import Dashboard from "../dashboard/Dashboard";
 
 //shows the controls and display
+
+//default state
+test("state unlocked and open", () => {
+  const { getByText } = render(<Dashboard />);
+  expect(getByText("Close Gate"));
+  expect(getByText("Lock Gate"));
+  expect(getByText("Open"));
+  expect(getByText("Unlocked"));
+});
+
+// Testing all the unlock, open, locked and close
+// test("does all the unlock states work?", () => {
+// //render
+// const {getByText, queryByText} = render(Dashboard/>)
+// //
+
+// })
